102459c7a6ed7eebe1dc4ba7f74ec71a0f8e0702
Rahul Bhatt <rbhatt.cs@gmail.com>
2025-04-02 21:09:12 +0530

    adding array file

---
+++ Arrays.js
@@ -0,0 +1,24 @@
+// arrays
+
+// const myarray = [0,1,2,3,4,5,"Rahul",true]
+// const myArr = [0,1,2,3,4,5]
+// console.log(myArr)
+// array copy creates shallow copies(means that if you do changes in the copy then original will also get changed)
+
+// deep copis: don't share same reference which means copy changes don't apply to original object.
+
+// const arr = new Array(0,1,2)
+// console.log(arr)
+
+
+// Array methods
+
+
+// push adds an element and pop removes an element from the last.
+// const testarr = new Array(0,1,2)
+// testarr.push(6);
+// testarr.pop()
+
+
+console.log(testarr)
+
