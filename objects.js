c79d4e91d5121fec92430ca8b8c7670cb4c50d4f
Rahul Bhatt <rbhatt.cs@gmail.com>
2025-04-03 13:48:02 +0530

    updating filees

---
+++ objects.js
@@ -0,0 +1,22 @@
+// Singleton is created as:
+// Object.create
+
+// object literal are:
+
+// using symbols in object declaration
+const mysym = Symbol("key")
+
+const obj = {
+    name : "Rahul",
+    [mysym]: "key 1",
+    age : 19,
+    location : "Haldwani",
+    email : "rahul@google.com",
+    isLoggedin:false
+}
+
+console.log(obj[mysym])
+
+console.log(obj.email)
+// When we give '.' we not need to give the particular syntax. but when we use the array form we need to specify that the feild is a string.
+console.log(obj["email"])
