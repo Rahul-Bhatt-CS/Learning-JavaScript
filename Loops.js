1bd86116174567619105703d9404624649af1693
Rahul-Bhatt-CS <rbhatt.cs@gmail.com>
2025-04-03 22:16:47 +0530

    Create Loops.js

---
+++ Loops.js
@@ -0,0 +1,22 @@
+// we can create a for in loop and a for of loop 
+
+// const arr = [1,2,3,4,5]
+// here we will get the indexs when we print key
+// for(const key in arr){
+// }
+
+// here we will get the value in arr when we print key
+// for(const key of arr){
+// }
+
+
+// object ke liye hamesha for in loop use hota hai aur agar tumhe us key ki value chahiye to ${object_name[key]} is tarah se use kr skte ho
+
+
+// agar ek array me hr element ke liye loop lagana hai to uske liye shprtcut:
+
+// arr_name.foreach( function (item_name_to_use_all_the_elements_of_array) {
+//   function declared here
+// })
+
+// can also use an arrow function for this so that it is easier to understand
