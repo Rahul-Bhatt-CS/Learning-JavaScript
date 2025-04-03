e607afb45093828baf3c7984a7d6606b6f79089e
Rahul-Bhatt-CS <rbhatt.cs@gmail.com>
2025-04-03 23:08:30 +0530

    Create filter.js

---
+++ filter.js
@@ -0,0 +1,15 @@
+// when we use the for each loop we cannot return any values from that but if we use filter then we can return values of each element of array:
+
+// const myarr = [0,1,2,3,4,5,6]
+// const newnums = myarr.filter( (num) => {
+ // return num>=0
+// })
+// console.log(newnums)
+// in this way we get all the values of array in a new array but only those values which satisfy a certain condition
+
+
+
+
+// we have filter 
+//         map
+//         and reduce to use as a chain and these all are an edvanced versions of foreach loop
