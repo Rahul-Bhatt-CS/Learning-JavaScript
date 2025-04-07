2e71c954b163a2a27643f4cc525d93414050b32e
Rahul Bhatt <rbhatt.cs@gmail.com>
2025-04-07 13:32:25 +0530

    adding practice project

---
+++ Practice projects/script.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const w = parseInt(document.querySelector('#weight').value)
+    const h = parseInt(document.querySelector('#height').value)
+})
